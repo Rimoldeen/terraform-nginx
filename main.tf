6ad32272915e2ca30553bb994951aab5a2478a49
Rimoldeen <rimoldeenfrancis@gmail.com>
2024-08-20 14:18:29 +0100

    Added missing Logging for ECS - Also tested with ECR image

--- main.tf
+++ main.tf
@@ -1,3 +1,9 @@
+resource "aws_cloudwatch_log_group" "ecs_task_log_group" {
+  name = "ecs-nginx-task-log-group"
+}
+
+data "aws_region" "current" {}
+
 module "vpc" {
   source       = "./modules/terraform-aws-vpc"
   project_name = "ecs-nginx-task"
@@ -9,6 +15,45 @@ module "alb" {
   public_subnets = module.vpc.public_subnets
   project_name   = "ecs-nginx-task"
 }
+
+# Deploy From an ECR Image Example - Basic Vite JS App 
+
+# module "ecs" {
+#   source           = "./modules/terraform-aws-ecs"
+#   vpc_id           = module.vpc.vpc_id
+#   private_subnets  = module.vpc.private_subnets
+#   target_group_arn = module.alb.target_group_arn
+
+#   project_name = "ecs-nginx-task"
+
+
+#   // Basic nginx image
+#   container_name = "frontend"
+#   container_definitions = jsonencode([
+#     {
+#       name  = "frontend"
+#       image = "113439214745.dkr.ecr.eu-west-2.amazonaws.com/frontend:latest"
+#       portMappings = [
+#         {
+#           containerPort = 80
+#           hostPort      = 80
+#         }
+#       ],
+
+#       logConfiguration = {
+#         logDriver = "awslogs"
+#         options = {
+#           awslogs-create-group  = "true"
+#           awslogs-group         = aws_cloudwatch_log_group.ecs_task_log_group.name
+#           awslogs-region        = data.aws_region.current.name
+#           awslogs-stream-prefix = "ecs-nginx-task"
+#         }
+#       }
+#     }
+#   ])
+# }
+
+# Deploy From Docker Hub Image Example - Basic Nginx App
 
 module "ecs" {
   source           = "./modules/terraform-aws-ecs"
@@ -30,7 +75,16 @@ module "ecs" {
           containerPort = 80
           hostPort      = 80
         }
-      ]
+      ],
+      logConfiguration = {
+        logDriver = "awslogs"
+        options = {
+          awslogs-create-group  = "true"
+          awslogs-group         = aws_cloudwatch_log_group.ecs_task_log_group.name
+          awslogs-region        = data.aws_region.current.name
+          awslogs-stream-prefix = "ecs-nginx-task"
+        }
+      }
     }
   ])
 }
